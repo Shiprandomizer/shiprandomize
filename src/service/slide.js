e48951b619e12c97452c5fa8e8a626cb1dd33413
Mark Lechtermann <mlechtermann@dspace.de>
2023-01-03 22:56:36 +0100

    refactor: cleanup

--- src/service/slide.js
+++
@@ -1,8 +0,0 @@
-function toggleInfoPanel() {
-    var panel = document.querySelector('.info-panel');
-    if (panel.style.display === 'block') {
-      panel.style.display = 'none';
-    } else {
-      panel.style.display = 'block';
-    }
-  }
